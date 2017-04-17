031838f3771acff7258d77d0038c3fb0f0061e9f
carlganz <carlganz@gmail.com>
2017-04-17 04:44:45 +0000

    shiny ajax stolen from Dean

---
+++ R/api.R
@@ -0,0 +1,51 @@
+api.certificate <- function(params) {
+  list(certificate = Sys.getenv("qz_certificate"),
+       success = TRUE)
+}
+
+api.signature <- function(params) {
+  toSign <- params$toSign
+  return(list(signature = qz_signature(toSign),
+              success = TRUE))
+  
+}
+
+#' @export
+#' @import shiny
+#' @rdname qz_libs
+server_api <- function(session) {
+  # Dean Attali, July 2015
+  
+  # listen for when javascript makes an api call
+  shiny::observeEvent(session$input[['api']], {
+    # grab all the call parameters
+    params <- session$input[['api']]
+    # determine what R method to call
+    method <- params[['_method']]
+    method <- sprintf("api.%s", method)
+    
+    # save the request ID
+    reqid  <- params[['_reqid']]
+    
+    # remove the meta params from the param list
+    params[['_method']] <- NULL
+    params[['_reqid']]  <- NULL
+    
+    # attempt to run the R function and return the response, along with request ID
+    tryCatch({
+      response <- do.call(method, as.list(list(params)))
+      response <- as.list(response)
+      response['_reqid'] <- reqid
+      
+      session$sendCustomMessage(type = "api.callback", response)
+      
+    },
+    # if an error occurs, call the error callback
+    error = function(err) {
+      response <- list(message = err$message,
+                       `_reqid`  = reqid)
+      session$sendCustomMessage(type = "api.failureCallback", response)
+      
+    })
+  })
+}
