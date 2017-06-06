76114cc293e82713719d1686b61a687b2e0abb0e
carlganz <carlganz@gmail.com>
2017-06-06 10:59:20 -0700

    separate init functions

--- R/server-functions.R
+++ R/server-functions.R
@@ -3,22 +3,26 @@
 #' @param session The \code{session} object passed to function given to
 #'   \code{shinyServer}
 #' @export
-qz_launch <- function(session) {
+#' @importFrom shinyjs delay
+qz_init <- function(session) {
   session$sendCustomMessage("qz_init", list())
-  shinyjs::delay(5000, {
-    session$sendCustomMessage("qz_connect", list(bool=TRUE))
-  })
 }
 
 #' @export
-#' @rdname qz_launch
+#' @rdname qz_init
+qz_connect <- function(session) {
+  session$sendCustomMessage("qz_connect", list())
+}
+
+#' @export
+#' @rdname qz_init
 
 list_printers <- function(session) {
   session$sendCustomMessage("list_printers", list())
 }
 
 #' @export
-#' @rdname qz_launch
+#' @rdname qz_init
 
 list_hid <- function(session) {
   session$sendCustomMessage("list_hid", list())
@@ -27,7 +31,7 @@ list_hid <- function(session) {
 #' @export
 #' @param vendor hex code for vendorId
 #' @param product hex code for productId
-#' @rdname qz_launch
+#' @rdname qz_init
 
 hid_read <- function(session, vendor, product) {
   session$sendCustomMessage("hid_read", list(vendor = vendor, product = product))
